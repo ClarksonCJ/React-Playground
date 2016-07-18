2941222b0460a0b6539ea2fbe6efc3fba19faeab
Chris Clarkson <chris.clarkson@cjcsoftwaresolutions.co.uk>
2016-07-18 21:49:19 +0100

    Lane Functionality

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,76 +1,27 @@
 import React from 'react';
 import uuid from 'uuid';
-import Notes from './Notes';
+import connect from '../libs/connect';
+import Lanes from './Lanes';
+import LaneActions from '../actions/LaneActions';
 
-export default class App extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            notes: [
-                {
-                    id: uuid.v4(),
-                    task: 'Learn React'
-                },
-                {
-                    id: uuid.v4(),
-                    task: 'Do Laundry'
-                }
-            ]
-        };
-    }
-    render() {
-        const {notes} = this.state;
-
-        return (
-            <div>
-                <button className="add-note" onClick={this.addNote}>+</button>
-                <Notes
-                    notes={notes}
-                    onNoteClick={this.activateNoteEdit}
-                    onEdit={this.editNote}
-                    onDelete={this.deleteNote}
-                    />
-            </div>
-        );
-    }
-
-    addNote = () => {
-        this.setState({
-            notes: this.state.notes.concat([{
-                id: uuid.v4(),
-                task: 'New Task'
-            }])
+const App = ({LaneActions, lanes}) => {
+    const addLane = () => {
+        LaneActions.create({
+            id: uuid.v4(),
+            name: 'New Lane'
         });
-    }
+    };
 
-    deleteNote = (id, e) => {
-        e.stopPropagation();
+    return(
+        <div>
+            <button className="add-lane" onClick={addLane}>+</button>
+            <Lanes lanes={lanes} />
+        </div>
+    );
+};
 
-        this.setState({
-            notes: this.state.notes.filter(note => note.id !== id)
-        });
-    }
-
-    activateNoteEdit = (id) => {
-        this.setState({
-            notes: this.state.notes.map(note => {
-                if(note.id === id){
-                    note.editing = true;
-                }
-                return note;
-            })
-        });
-    }
-
-    editNote = (id, task) => {
-        this.setState({
-            notes: this.state.notes.map(note => {
-                if(note.id === id){
-                    note.editing = false;
-                    note.task = task;
-                }
-                return note;
-            })
-        });
-    }
-}
+export default connect(({lanes}) => ({
+    lanes
+}), {
+    LaneActions
+})(App)
