93f7354453e8ec01297414d5621b18a486298369
Chris Clarkson <chris.clarkson@cjcsoftwaresolutions.co.uk>
2016-07-18 22:12:23 +0100

    Styling kanban

--- app/actions/LaneActions.js
+++ app/actions/LaneActions.js
@@ -1,5 +1,9 @@
 import alt from '../libs/alt';
 
 export default alt.generateActions(
-    'create', 'attachToLane', 'detachFromLane'
+    'create',
+    'update',
+    'delete',
+    'attachToLane',
+    'detachFromLane'
 );
